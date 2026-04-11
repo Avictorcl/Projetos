bf18245814eb2ff0b4294d8b025bdf188af50517
Avictorcl <andresvictorcallegari@gmail.com>
2026-04-11 02:03:39 -0300

    estudoAlgoritmos

---
+++ Algoritimos/Prova1_Algoritimos/structs.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+
+//? Structs básicos:
+void struct_1() {
+    
+    struct Carro {
+        char modelo[50];
+        int ano;
+        int potencia;
+    };
+
+    struct Carro meuVeiculo;
+
+    strcpy(meuVeiculo.modelo, "Onix");
+    meuVeiculo.ano = 2025;
+    meuVeiculo.potencia = 116;
+
+    printf("Veiculo registrado: %s, ano %d, com %d cavalos de potencia.\n", 
+           meuVeiculo.modelo, meuVeiculo.ano, meuVeiculo.potencia);
+
+}
+
+//? Typedef:
+void struct_2(){
+    // ==================================//
+    // CASO 1: TYPEDEF COM TIPO BÁSICO   //
+    //===================================//
+
+    /* O que faz: Avisa o sistema que a partir de agora, a palavra "Inteiro"
+       siginifica exatamente a mesma coisa que "int" [Cite: 186]          */
+       typedef int Inteiro;
+    
+    // Usando o novo "apelido"
+        Inteiro nivelBateria = 85;
+        printf("O nivel da bateria está em %d%% \n", nivelBateria);
+
+
+    //=====================================================//
+    // CASO 2: TYPEDEF COM STRUCTS (A VERDADEIRA UTILIDADE)//
+    //=====================================================//
+
+    /*O que faz: Cria uma estrutura imediatamente a batiza com o apelido "Sensor"
+    Note que não escrevemos struct Sensor ( nós colocamos o nome la no final,
+    antes do ponto e virgula)                                                  */
+
+    typedef struct {
+        char modelo[50];
+        int porta_leitura;
+    } Sensor;
+
+    /* Criamos a variavel apenas usando o apeelido dela.
+        Veja como fica limpo, não é preciso escrever "struct Sensor meuSensor"*/
+
+        Sensor sensorTemperatura;
+
+        strcpy (sensorTemperatura.modelo, "Termopar Tipo K");
+        sensorTemperatura.porta_leitura = 4;
+
+        printf("Equipamento : %s, está medindo: %d", sensorTemperatura.modelo, sensorTemperatura.porta_leitura);
+
+}
+
+void struct_3(){
+    
+}
+// PAINEL DE CONTROLE
+int main() {
+    struct_1();
+    return 0;
+}
